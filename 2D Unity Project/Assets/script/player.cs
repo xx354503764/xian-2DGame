cdc15e0eccd0ebbc07983882a3bf0c652fc0ff19
xx354503764 <56706566+xx354503764@users.noreply.github.com>
2019-11-08 12:00:53 +0800

    建立脚本定义栏位

---
+++ 2D Unity Project/Assets/script/player.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    [Header ("速度")][Range (0f,100f)]
+    public float speed = 3.5f;
+    [Header ("跳跃"),Range(100,2000)]
+    public int jump = 300;
+    [Header("是否在地板上"),Tooltip("用来判定角色是否在地板上。")]
+    public bool isGround = false;
+    [Header("角色名称")]
+    public string name = "弦";
+
+}
