4e80535c9825fb35b739ae66706fa15873e1eb73
xx354503764 <56706566+xx354503764@users.noreply.github.com>
2019-11-15 12:16:28 +0800

    程序动画控制

--- 2D Unity Project/Assets/script/player.cs
+++ 2D Unity Project/Assets/script/player.cs
@@ -10,5 +10,44 @@ public class player : MonoBehaviour
     public bool isGround = false;
     [Header("角色名称")]
     public string name = "弦";
+    [Header("元件")]
+    public Rigidbody2D r2d;
+    public Animator ani;
 
+    private void Move()
+    {
+        float h = Input.GetAxisRaw("Horizontal");  // 输入，取得轴向（水平），左右与AD。
+        r2d.AddForce(new Vector2(speed * h , 0));
+        ani.SetBool("跑步开关",h != 0);
+    }
+
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGround == true)
+        {
+            isGround = false;
+            r2d.AddForce(new Vector2(0,jump));
+        }
+    }
+
+    private void  Dead()
+    {
+
+    }
+
+    //事件：在特定事件点以指定次数执行
+    //更新事件：一秒执行约60次(60fps)
+    private void Update()
+    {
+        Move();
+    }
+
+    private void  OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.name=="地板")
+        {
+            isGround = true;
+        }
+    }
+    
 }
